5157ea9daa6f4627437e28e76e515aa796b32fbe
Bella Evalle <bellaevalle@yahoo.com>
2020-08-10 15:50:02 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,10 +11,8 @@ attr_accessor :name, :roster
   
   def add_student(student_name, grade)
     if @roster[grade] 
-      puts "Already here"
       @roster[grade] << student_name
     else 
-      puts "Created a new grade"
       @roster[grade] = [student_name]
     end
   end
