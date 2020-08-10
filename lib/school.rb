1cf04f579f02908faecd09be21d80d0d4909bc0b
Bella Evalle <bellaevalle@yahoo.com>
2020-08-10 16:15:02 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+require 'pry'
+
+class School
+
+attr_accessor :name, :roster
+
+  def initialize (name_arg)
+    @name = name_arg
+    @roster = {}
+  end
+  
+  def add_student(student_name, grade)
+    if @roster[grade] 
+      @roster[grade] << student_name
+    else 
+      @roster[grade] = [student_name]
+    end
+  end
+  
+  def grade(student_grade)
+    roster[student_grade]
+  end
+  
+  def sort
+   @roster.each do |grade, student_array|
+      student_array.sort!
+      end
+      @roster
+  end
+  
+end
